d5fc6961c5377b2f5737c450aa4d2746e4141307
abdo <abrab93@gmail.com>
2020-01-10 15:59:05 +0100

    adding routes Guards

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Authentication from './containers/Auth/Auth';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 import { connect } from 'react-redux';
@@ -27,22 +27,43 @@ class App extends Component {
   }
 
   render() {
+
+    let routes = (
+      <Switch>
+        <Route path='/auth' component={Authentication} />
+        <Route path='/' exact component={BurgerBuilder} />
+        <Redirect to='/' />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path='/orders' component={Orders} />
+          <Route path='/checkout' component={Checkout} />
+          <Route path='/logout' component={Logout} />
+          <Route path='/' exact component={BurgerBuilder} />
+          <Redirect to='/' />
+        </Switch>
+      );
+    }
+
     return (
       <div>
         <Layout>
           {/* {this.state.show ? <BurgerBuilder /> : null} */}
           {/* <BurgerBuilder /> */}
           {/* <Checkout /> */}
-          <Switch>
-            <Route path='/orders' component={Orders} />
-            <Route path='/checkout' component={Checkout} />
-            <Route path='/auth' component={Authentication} />
-            <Route path='/logout' component={Logout} />
-            <Route path='/' exact component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
   }
 }
 
@@ -52,4 +73,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
