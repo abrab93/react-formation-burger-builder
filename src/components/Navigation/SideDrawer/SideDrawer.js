9c20c7c197348771d295367730f8e7efea919609
abdo <abrab93@gmail.com>
2020-01-13 10:54:48 +0100

    UI improvement 2

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -14,12 +14,12 @@ const sideDrawer = (props) => {
     return (
         <Aux>
             <Backdrop show={props.open} clicked={props.closed} />
-            <div className={assinedClasses.join(' ')}>
+            <div className={assinedClasses.join(' ')} onClick={props.closed}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
                 <nav>
-                    <NavigationItems isAuthenticated={props.isAuth} />
+                    <NavigationItems isAuthenticated={props.isAuth} clicked={props.closed} />
                 </nav>
             </div>
         </Aux>
